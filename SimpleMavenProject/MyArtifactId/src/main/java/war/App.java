9bd5797129c0328e95d1b23e693a131a97709a43
chiplunkar-shubham <shubh.chiplunkar@gmail.com>
2022-06-06 21:59:11 +0530

    SimpleMavenProject

---
+++ SimpleMavenProject/MyArtifactId/src/main/java/war/App.java
@@ -0,0 +1,13 @@
+package war;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My Name is Shubham Chiplunkar, and this is a simple Maven Hello World Project" );
+    }
+}
